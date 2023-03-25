9641d3ac4b5af7305b410f1ae72b26d2797ba9f3
shas232 <79205176+shas232@users.noreply.github.com>
2023-03-26 01:30:44 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,89 @@
+
+from imutils.video import VideoStream
+from imutils.video import FPS
+import numpy as np
+import argparse
+import imutils
+import time
+import cv2
+import win32com.client
+
+'''ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--prototxt", required=True,
+	help="path to Caffe 'deploy' prototxt file")
+ap.add_argument("-m", "--model", required=True,
+	help="path to Caffe pre-trained model")
+ap.add_argument("-c", "--confidence", type=float, default=0.2,
+	help="minimum probability to filter weak detections")
+args = vars(ap.parse_args())'''
+
+CLASSES = [
+    "bus", "car", "chair", "motorbike", "person", ]
+COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
+
+print("[INFO] loading model...")
+net = cv2.dnn.readNetFromCaffe(
+    'MobileNetSSD_deploy.prototxt.txt', 'MobileNetSSD_deploy.caffemodel')
+
+print("[INFO] starting video stream...")
+vs = VideoStream(src=1).start()
+time.sleep(2.0)
+fps = FPS().start()
+
+while True:
+    frame = vs.read()
+    frame = imutils.resize(frame, width=400)
+
+    (h, w) = frame.shape[:2]
+    blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)),
+                                 0.007843, (300, 300), 127.5)
+
+    net.setInput(blob)
+    detections = net.forward()
+
+    for i in np.arange(0, detections.shape[2]):
+        confidence = detections[0, 0, i, 2]
+
+        if confidence > 0.2:
+            idx = int(detections[0, 0, i, 1])
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (startX, startY, endX, endY) = box.astype("int")
+
+            label = "{}".format(CLASSES[idx])
+            cv2.rectangle(frame, (startX, startY), (endX, endY),
+                          COLORS[idx], 2)
+            y = startY - 15 if startY - 15 > 15 else startY + 15
+            cv2.putText(frame, label, (startX, y),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
+            distancei = (2 * 3.14 * 180) / (endX - startX +
+                                            (endY-startY) * 360) * 1000 + 3
+            dist2 = int(distancei)
+            dist2 = ("{:.2f}" .format(dist2 * 0.0254))
+            print(label, ":", distancei, "metres")
+            cx = startX + endX / 2
+            cy = startY + endY / 2
+
+            if (cx < frame.shape[1]) / 2:
+                print("Object is on the left")
+            else:
+                print("Object is on the right")
+            if (label == "car" or label == "motorbike"):
+                str1 = label+"detected at"+str(dist2)
+                speaker = win32com.client.Dispatch("SAPI.SpVoice")
+
+                speaker.Speak(str1)
+
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1) & 0xFF
+
+    if key == ord("q"):
+        break
+
+    fps.update()
+
+fps.stop()
+print("[INFO] elapsed time: {:.2f}".format(fps.elapsed()))
+print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+
+cv2.destroyAllWindows()
+vs.stop()
